57b21f0d11182ea99b7d26cbdc68dfab7627b741
Master Lao <4nbrz32x6ob9@mail.ru>
2024-09-16 01:01:22 +0300

    add github pages

--- tests/library.test.ts
+++ tests/library.test.ts
@@ -1,5 +1,12 @@
 import { test } from '@fixtures/fixtures';
+import { LoginPageSteps } from '@steps/login.steps';
 
-test('Open page login, click library - library contains correct html book', async ({ app }) => {
-  await app.loginPage.openPage();
+// test('Open page login, click library - library contains correct html book', async ({ app }) => {
+//   await app.loginPage.openPage();
+// });
+
+test('Dummy test - should be deleted', async ({ page, context }) => {
+  const loginPage = new LoginPageSteps(page, context);
+  await loginPage.openPage();
+  await loginPage.assertLoginButtonIsVisible();
 });
